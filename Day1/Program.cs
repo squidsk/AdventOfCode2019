4df75bfac2568a49924cfbb77980ec089c5b43ba
Squid <squidsk@gmail.com>
2019-12-10 00:23:41 -0700

    Day 1

---
+++ Day1/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Steve
+ * Date: 12/9/2019
+ * Time: 11:29 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.IO;
+
+namespace Day1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            using(StringReader reader = new StringReader(File.ReadAllText("input.txt"))) {
+                int totalFuel = 0;
+                int currentModuleFuel = 0;
+                string moduleMass;
+                while((moduleMass = reader.ReadLine()) != null) {
+                    currentModuleFuel = (int.Parse(moduleMass) / 3) - 2;
+                    totalFuel += currentModuleFuel;
+                }
+                Console.WriteLine(string.Format("Total Fuel Cost is: {0}", totalFuel));
+            }
+            
+            
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Steve
+ * Date: 12/9/2019
+ * Time: 11:28 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace AdventOfCode2019
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            // TODO: Implement Functionality Here
+            
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
