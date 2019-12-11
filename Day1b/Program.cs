21e81aceb6c87abc0ec9ee740fd63402f01274a4
Squid <squidsk@gmail.com>
2019-12-10 20:23:55 -0700

    Day1b and Day2

---
+++ Day1b/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Steve
+ * Date: 12/10/2019
+ * Time: 8:40 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.IO;
+
+namespace Day1b
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            using(StringReader reader = new StringReader(File.ReadAllText("input.txt"))) {
+                int totalFuel = 0;
+                int currentModuleFuel = 0;
+                string moduleMassAsString;
+                while((moduleMassAsString = reader.ReadLine()) != null) {
+                    int moduleMass = int.Parse(moduleMassAsString);
+                    while(moduleMass >= 6) {
+                        currentModuleFuel = ( moduleMass / 3) - 2;
+                        //Console.WriteLine(string.Format("Mass: {0}, Fuel {1}", moduleMass, currentModuleFuel));
+                        totalFuel += currentModuleFuel;
+                        moduleMass = currentModuleFuel;
+                    }
+                }
+                Console.WriteLine(string.Format("Total Fuel Cost is: {0}", totalFuel));
+            }
+            
+            
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Day2/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Steve
+ * Date: 12/10/2019
+ * Time: 1:48 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Day2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string fileContents = File.ReadAllText("input.txt");
+            string[] temp = fileContents.Split(',');
+            int[] code = new int[temp.Length];
+            
+            for(int i = 0; i < temp.Length; i += 1) {
+                code[i] = int.Parse(temp[i]);
+            }
+            
+            code[1] = 12;
+            code[2] = 2;
+            
+            for(int i = 0; i < code.Length; i += 4) {
+                if(code[i] == 1) {
+                    code[code[i+3]] = code[code[i+1]] + code[code[i+2]];
+                } else if (code[i] == 2) {
+                    code[code[i+3]] = code[code[i+1]] * code[code[i+2]];
+                } else //if(code[i] == 99)
+                    break;
+            }
+            Console.WriteLine(string.Format("The value at position 0 is: {0}", code[0]));
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
